ac2e4ab0d75ff80fad52c357d4ca8d7e4565c9ac
skelletons <skelletons@gmail.com>
2015-03-19 01:14:54 +0100

    Cleaning up main

--- main.cpp
+++ main.cpp
@@ -29,7 +29,7 @@ int compare(const void* pi1, const void* pi2)
 int main()
 {
     int i = 0;
-    char Tableau [48][4]= {"2t","2ca","2co","2p","3t","3ca","3co","3p","4t","4ca","4co","4t","5t","5ca","5co","5p","6t","6ca","6o","6p","7t","7ca","7co","7p","8t","8ca","8co","8p","9t","9ca","9co","9p","Vt","Vca","Vco","Vp","Dt","Dca","Dco","Dp","Rt","Rca","Rco","Rp","At","Aca","Aco","Ap"};
+    char tab[48][4]= {"2t","2ca","2co","2p","3t","3ca","3co","3p","4t","4ca","4co","4t","5t","5ca","5co","5p","6t","6ca","6o","6p","7t","7ca","7co","7p","8t","8ca","8co","8p","9t","9ca","9co","9p","Vt","Vca","Vco","Vp","Dt","Dca","Dco","Dp","Rt","Rca","Rco","Rp","At","Aca","Aco","Ap"};
     Deck deck[48];
 
     HCRYPTPROV hProvider = 0;
@@ -55,10 +55,10 @@ int main()
 
     for(i=0; i<48; i++)
     {
-        deck[i].name[0] = Tableau[i][0];
-        deck[i].name[1] = Tableau[i][1];
-        deck[i].name[2] = Tableau[i][2];
-        deck[i].name[3] = Tableau[i][3];
+        deck[i].name[0] = tab[i][0];
+        deck[i].name[1] = tab[i][1];
+        deck[i].name[2] = tab[i][2];
+        deck[i].name[3] = tab[i][3];
         deck[i].value = genrand_close_open();
     }
 
